e6e8f8d104de33e865941489a275240b708a7ee3
sandrasunil936 <sandrasunil676@gmail.com>
2026-06-16 19:05:03 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,25 @@
+#comments
+#variables
+my_age=25
+my_age=22
+my_age=23
+print(my_age)
+
+a = 3
+b = 7
+c = a
+a = b
+b = c 
+print(a)
+print(b)
+# swap 2 numbers 
+
+# operators
+
+a = 5
+b = 7
+a = a + b
+b = a - b
+a = a - b
+print (a)
+print (b)
