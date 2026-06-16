f1934c750992f5aec8669b5d6506babc0eadb92b
sandrasunil936 <sandrasunil676@gmail.com>
2026-06-16 19:06:02 +0530

    Add files via upload

---
+++ four.py
@@ -0,0 +1,87 @@
+a = 3
+b = 4
+
+print (a==b)
+
+print (a!=b)
+
+print (a<b)
+
+print (a>b)
+
+print (a>=b)
+
+print (a<=b)
+
+
+# coditional statements 
+# if condition :
+     # code to be executed 
+
+
+age = 18 
+ 
+
+if age >= 18:
+     print ("eligible")
+else:
+      print("not eligible")
+
+
+a = 5
+
+
+if  a % 2 == 0:
+      print("even")
+else:
+      print("odd")  
+
+
+num = 4555
+
+if num % 5 == 0:
+      print ("multiple of 5")
+
+a = 5 
+b = 7
+
+if a > b:
+      print ("a is the largest")
+else:
+      print ("b is the largest")
+
+
+ # else if 
+
+mark = 35 
+if mark > 70:
+      print("B+")
+elif mark > 60 :
+      print ("B")
+elif mark > 50 :
+      print ("C+")
+elif mark > 40 :
+      print (C)  
+else : 
+      print ("fail") 
+
+
+
+num = 34565
+
+if num > 0:
+      print ("num is positive")
+elif num < 0:
+      print ("num is negative")
+else:
+      print("zero")                     
+      
+      
+   
+
+
+         
+
+
+
+    
