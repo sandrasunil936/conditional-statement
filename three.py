23f4268e91618ac3e99924295ba407df46b9931e
sandrasunil936 <sandrasunil676@gmail.com>
2026-06-16 19:05:32 +0530

    Add files via upload

---
+++ three.py
@@ -0,0 +1,37 @@
+# datatypes
+a = 6
+name = "mohan"
+print(type(name))
+
+
+a = 0
+print(a)
+print(type(a))
+
+a = 5.0 
+print(a)
+print(type(a))
+
+a = True
+b = False
+print(a)
+print(bool(a))
+print(b)
+print(bool(b))
+
+a = 4
+b = 2 
+
+print(a+b) 
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a**b)
+
+print(a//b)
+
+print(a%b)
